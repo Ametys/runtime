753e560c03ba236b751958f8162fa21623ca50f1
Raphaël Franchet <raphael.franchet@ametys.org>
2008-07-21 10:23:55 +0200

    Warnings remove

--- test/src/org/ametys/runtime/test/users/jdbc/ModifiableCredentialAwareJdbcUsersTestCase.java
+++ test/src/org/ametys/runtime/test/users/jdbc/ModifiableCredentialAwareJdbcUsersTestCase.java
@@ -469,7 +469,7 @@ public class ModifiableCredentialAwareJdbcUsersTestCase extends JdbcUsersTestCas
         assertEquals(user.getEmail(), "email@email.ma");
     }
     
-    private void _checkListener(MyUserListener listener1, MyUserListener listener2, int added, int updated, int removed) throws Exception
+    private void _checkListener(MyUserListener listener1, MyUserListener listener2, int added, int updated, int removed)
     {
         assertEquals(added, listener1.getAddedUsers().size());
         assertEquals(updated, listener1.getUpdatedUsers().size());
