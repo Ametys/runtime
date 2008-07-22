55219daab8fac530a9f9acabd340ce5a523c9068
Sébastien Launay <slaunay@sierrawireless.com>
2008-07-22 14:53:33 +0200

    AMRUN-180 Update build task for publishing ivy artifacts Build does not fail anymore if the tests failed (Hudson compliant)

---
+++ deliver/src/org/ametys/runtime/deliver/IvyVersionTask.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2007 Anyware Technologies and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.opensource.org/licenses/eclipse-1.0.php
+ * 
+ * Contributors:
+ *     Anyware Technologies - initial API and implementation
+ */
+package org.ametys.runtime.deliver;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+/**
+ * Simple task for creating IVY version depending on:
+ * <ul>
+ *  <li>build version
+ *  <li>current SVN revision
+ *  <li>IVY status
+ *  <li>build timestamp
+ * </ul>
+ * 
+ * The resulting IVY version is:
+ * <dl>
+ *   <dt>release</dt>
+ *   <dd>[major].[minor].[patch]</dd>
+ *   <dt>integration</dt>
+ *   <dd>[major].[minor].[patch]-[svn.revision]-dev</dd>
+ *   <dt>development</dt>
+ *   <dd>[major].[minor].[patch]-[svn.revision]-dev-[timestamp] </dd>
+ * </dl>
+ * 
+ * The resolver to use (depending on the status) is also exported.
+ */
+public class IvyVersionTask extends Task
+{
+    private String _versionOutputProperty;
+    private String _resolverOutputProperty;
+    private String _buildVersion;
+    private String _svnRevision;
+    private String _status;
+    private String _timestamp;
+    
+    @Override
+    public void execute() throws BuildException
+    {
+    	String resolver = "ametys-dev";
+    	StringBuilder ivyVersion = new StringBuilder();
+    	
+    	ivyVersion.append(_buildVersion);
+    	
+    	if (!"release".equals(_status))
+    	{
+    		ivyVersion.append("-dev-");
+    		ivyVersion.append(_svnRevision);
+    		
+    		if (!"integration".equals(_status))
+	    	{
+        		ivyVersion.append("-");
+        		ivyVersion.append(_timestamp);
+	    	}
+    	}
+    	else
+    	{
+    		resolver = "ametys-release";
+    	}
+    	
+    	getProject().setProperty(_versionOutputProperty, ivyVersion.toString());
+    	getProject().setProperty(_resolverOutputProperty, resolver);
+    }
+    
+    /**
+     * Set the property to populate for IVY version value.
+     * @param name the property name.
+     */
+    public void setVersionOutputProperty(String name)
+    {
+    	_versionOutputProperty = name;
+    }
+    
+    /**
+     * Set the property to populate for resolver value.
+     * @param name the property name.
+     */
+    public void setResolverOutputProperty(String name)
+    {
+    	_resolverOutputProperty = name;
+    }
+
+    /**
+     * Set the build version.
+     * @param version the build version.
+     */
+    public void setBuildRevision(String version)
+    {
+		_buildVersion = version;
+	}
+
+    /**
+     * Set the SVN revision.
+     * @param revision the SVN revision.
+     */
+    public void setSvnRevision(String revision)
+    {
+		_svnRevision = revision;
+	}
+    
+    /**
+     * Set the IVY status.
+     * @param status the IVY status.
+     */
+    public void setStatus(String status)
+    {
+		_status = status;
+	}
+    
+    /**
+     * Set the build timestamp.
+     * @param timestamp the build timestamp.
+     */
+    public void setTimestamp(String timestamp)
+    {
+		_timestamp = timestamp;
+	}
+}
---
+++ deliver/src/org/ametys/runtime/deliver/SvnRevisionTask.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2007 Anyware Technologies and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.opensource.org/licenses/eclipse-1.0.php
+ * 
+ * Contributors:
+ *     Anyware Technologies - initial API and implementation
+ */
+package org.ametys.runtime.deliver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+/**
+ * Simple task for retrieving current SVN revision.
+ */
+public class SvnRevisionTask extends Task
+{
+    private String _property;
+    
+    @Override
+    public void execute() throws BuildException
+    {
+        try
+        {
+        	Process process = Runtime.getRuntime().exec("svnversion .");
+        	
+        	int statusCode = process.waitFor();
+        	String revision = "unknown";
+        	
+        	if (statusCode != 0)
+        	{
+        		
+        	}
+        	else
+        	{
+        		BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        		revision = reader.readLine();
+        		int index = revision.indexOf(":");
+
+        		if (index != -1)
+        		{
+        			revision = revision.substring(0, index);
+        		}
+        	}
+        	
+        	getProject().setProperty(_property, revision);
+        }
+        catch (Exception ex)
+        {
+            throw new BuildException(ex);
+        }
+    }
+    
+    /**
+     * Set the property to populate.
+     * @param name the property name.
+     */
+    public void setProperty(String name)
+    {
+        _property = name;
+    }
+}
