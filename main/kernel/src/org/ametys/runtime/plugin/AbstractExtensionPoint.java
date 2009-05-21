6fb2c98fdbd7e06e4a54c1ae46d9d85954201142
Cédric Damioli <cedric.damioli@ametys.org>
2009-05-21 21:34:40 +0200

    javadoc

--- main/kernel/src/org/ametys/runtime/plugin/AbstractExtensionPoint.java
+++ main/kernel/src/org/ametys/runtime/plugin/AbstractExtensionPoint.java
@@ -6,8 +6,17 @@ import java.util.Set;
 
 import org.apache.avalon.framework.activity.Initializable;
 
+/**
+ * Abstract implementation of an extension point.<br>
+ * Contains only helper methods. Tha actual job has to be done in the {@link #addExtension(String, String, org.apache.avalon.framework.configuration.Configuration)}
+ * @param <T> the type of the managed extensions
+ */
 public abstract class AbstractExtensionPoint<T> implements ExtensionPoint<T>, Initializable
 {
+    /**
+     * Map containing the extensions.<br>
+     * The key is the unique id of the extension.
+     */
     protected Map<String, T> _extensions;
     
     public void initialize() throws Exception
