5f792b421fecea47f61b35278a205d2813a32b6c
Cédric Damioli <cedric.damioli@ametys.org>
2009-12-18 22:15:13 +0100

    RUNTIME-374 Move to Apache License 2.0

--- main/plugin-core/src/org/ametys/runtime/plugins/core/ui/item/DesktopManager.java
+++ main/plugin-core/src/org/ametys/runtime/plugins/core/ui/item/DesktopManager.java
@@ -1,12 +1,17 @@
 /*
- * Copyright (c) 2007 Anyware Technologies and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.opensource.org/licenses/eclipse-1.0.php
- * 
- * Contributors:
- *     Anyware Technologies - initial API and implementation
+ *  Copyright 2009 Anyware Services
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package org.ametys.runtime.plugins.core.ui.item;
 
@@ -17,7 +22,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
-import org.apache.avalon.framework.component.Component;
 import org.apache.avalon.framework.configuration.Configurable;
 import org.apache.avalon.framework.configuration.Configuration;
 import org.apache.avalon.framework.configuration.ConfigurationException;
@@ -38,7 +42,7 @@ import org.ametys.runtime.ui.SAXClientSideElementHelper;
 /**
  * This manager handles the home page of spaces
  */
-public class DesktopManager extends AbstractThreadSafeComponentExtensionPoint<ClientSideElement> implements Configurable, Component
+public class DesktopManager extends AbstractThreadSafeComponentExtensionPoint<ClientSideElement> implements Configurable
 {
     /** helper to sax client side elements */
     protected SAXClientSideElementHelper _saxClientSideElementHelper;
