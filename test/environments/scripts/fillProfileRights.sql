127f6888085ebfa6d8bad701e500643e9f4c7030
Laurence Aumeunier <laurence.aumeunier@ametys.org>
2010-02-08 15:21:32 +0100

    Try to fix test case

--- test/environments/scripts/fillProfileRights.sql
+++ test/environments/scripts/fillProfileRights.sql
@@ -13,15 +13,15 @@ insert into Rights_ProfileRights (Profile_Id, Right_Id) values
 ((SELECT Id FROM Rights_Profile WHERE Id = last_insert_id()), 'right1'),
 ((SELECT Id FROM Rights_Profile WHERE Id = last_insert_id()), 'right2');
 insert into Rights_UserRights (Profile_Id, Login, Context) values
-((SELECT Id FROM Rights_Profile WHERE Id = last_insert_id()), 'test', '/test'),
-((SELECT Id FROM Rights_Profile WHERE Id = last_insert_id()), 'test', '/test2/test2');
+((SELECT Id FROM Rights_Profile WHERE Id = last_insert_id()), 'test', '/application/test'),
+((SELECT Id FROM Rights_Profile WHERE Id = last_insert_id()), 'test', '/application/test2/test2');
 insert into Rights_GroupRights (Profile_Id, Group_Id, Context) values
-((SELECT Id FROM Rights_Profile WHERE Id in (select max(Id) from Rights_Profile)), (SELECT Id FROM Groups WHERE Id in (select max(Id) from Groups)), '/test3');
+((SELECT Id FROM Rights_Profile WHERE Id in (select max(Id) from Rights_Profile)), (SELECT Id FROM Groups WHERE Id in (select max(Id) from Groups)), '/application/test3');
 
 insert into Rights_Profile(Label) value('Profil 2');
 insert into Rights_ProfileRights (Profile_Id, Right_Id) values 
 ((SELECT Id FROM Rights_Profile WHERE Id = last_insert_id()), 'right3');
 insert into Rights_UserRights (Profile_Id, Login, Context) values
-((SELECT Id FROM Rights_Profile WHERE Id in (select max(Id) from Rights_Profile)), 'test2', '/test2/test2'),
-((SELECT Id FROM Rights_Profile WHERE Id in (select max(Id) from Rights_Profile)), 'test', '/test3');
+((SELECT Id FROM Rights_Profile WHERE Id in (select max(Id) from Rights_Profile)), 'test2', '/application/test2/test2'),
+((SELECT Id FROM Rights_Profile WHERE Id in (select max(Id) from Rights_Profile)), 'test', '/application/test3');
 
