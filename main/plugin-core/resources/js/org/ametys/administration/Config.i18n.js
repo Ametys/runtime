7b071c5e5c37107652a421eb04e502cec6c2fdd0
Laurence Aumeunier <laurence.aumeunier@ametys.org>
2011-12-20 19:14:32 +0100

    RUNTIME-663 [Configuration parameters] Use alphabetical order by default for combo list

--- main/plugin-core/resources/js/org/ametys/administration/Config.i18n.js
+++ main/plugin-core/resources/js/org/ametys/administration/Config.i18n.js
@@ -391,7 +391,8 @@ org.ametys.administration.Config._createTextField = function (name, value, label
 	        store: new Ext.data.SimpleStore({
 	            id: 0,
 	            fields: [ 'value', 'text'],
-	            data: enumeration
+	            data: enumeration,
+	            sortInfo: {field: 'text'} // default order
 	        }),
 	        valueField: 'value',
 	        displayField: 'text'
