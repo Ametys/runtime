e077e30209a0cb7e41bcb01c5e12a92ac7059c4c
Raphaël Franchet <raphael.franchet@ametys.org>
2012-08-23 10:23:26 +0200

    Merged revisions 16635 via svnmerge from https://svn.ametys.org/trunk/runtime/trunk
    ........
      r16635 | raphael | 2012-08-23 10:22:12 +0200 (jeu., 23 ao?t 2012) | 1 line
      
      RUNTIME-761 Link in the Servercomm error screen is wrong
    ........

--- main/kernel/resources/js/org/ametys/servercomm/ServerComm.i18n.js
+++ main/kernel/resources/js/org/ametys/servercomm/ServerComm.i18n.js
@@ -293,7 +293,7 @@ org.ametys.servercomm.ServerComm.prototype._shutdown = function(m)
 	
 	document.body.style.backgroundColor = "#FFFFFF";
 	document.body.style.color = "#000000";
-	document.body.innerHTML = "&lt;h1&gt;" + "<i18n:text i18n:key='KERNEL_SERVERCOMM_LISTENERREQUEST_LOST_CONNECTION_1'/>" + "&lt;/h1&gt; &lt;p&gt;" + "<i18n:text i18n:key='KERNEL_SERVERCOMM_LISTENERREQUEST_LOST_CONNECTION_2'/>" +"&lt;/p&gt;&lt;p&gt;&lt;a href='index.html'&gt;" + "<i18n:text i18n:key='KERNEL_SERVERCOMM_LISTENERREQUEST_LOST_CONNECTION_3'/>" + "&lt;/a&gt;&lt;/p&gt;"
+	document.body.innerHTML = "&lt;h1&gt;" + "<i18n:text i18n:key='KERNEL_SERVERCOMM_LISTENERREQUEST_LOST_CONNECTION_1'/>" + "&lt;/h1&gt; &lt;p&gt;" + "<i18n:text i18n:key='KERNEL_SERVERCOMM_LISTENERREQUEST_LOST_CONNECTION_2'/>" +"&lt;/p&gt;&lt;p&gt;&lt;a href='javascript:location.reload(true);'&gt;" + "<i18n:text i18n:key='KERNEL_SERVERCOMM_LISTENERREQUEST_LOST_CONNECTION_3'/>" + "&lt;/a&gt;&lt;/p&gt;"
 }
 
 /**
