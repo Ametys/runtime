8d0df422631b869404ca0182ab80d4de1a2f0a04
Nicolas Gavalda <nicolas.gavalda@ametys.org>
2012-10-17 11:15:14 +0200

    RUNTIME-782: when using large groups (about 2000 users), users are sometimes lost when updating the group through the interface.

--- main/plugin-core/src/org/ametys/runtime/plugins/core/group/jdbc/ModifiableJdbcGroupsManager.java
+++ main/plugin-core/src/org/ametys/runtime/plugins/core/group/jdbc/ModifiableJdbcGroupsManager.java
@@ -549,19 +549,35 @@ public class ModifiableJdbcGroupsManager extends AbstractLogEnabled implements M
             
             statement.executeUpdate();
             ConnectionHelper.cleanup(statement);
-
-            Iterator userIt = userGroup.getUsers().iterator();
-            while (userIt.hasNext())
+            
+            // Test if the connection supports batch updates.
+            boolean supportsBatch = connection.getMetaData().supportsBatchUpdates();
+            
+            statement = connection.prepareStatement("INSERT INTO " + _groupsCompositionTable + " (" + _groupsCompositionColGroup + ", " + _groupsCompositionColUser + ") VALUES (?, ?)");
+            
+            for (String login : userGroup.getUsers())
             {
-                String login = (String) userIt.next();
-
-                statement = connection.prepareStatement("INSERT INTO " + _groupsCompositionTable + " (" + _groupsCompositionColGroup + ", " + _groupsCompositionColUser + ") VALUES (?, ?)");
                 statement.setInt(1, Integer.parseInt(userGroup.getId()));
                 statement.setString(2, login);
                 
-                statement.executeUpdate();
-                ConnectionHelper.cleanup(statement);
+                // If batch updates are supported, add to the batch, else execute directly.
+                if (supportsBatch)
+                {
+                    statement.addBatch();
+                }
+                else
+                {
+                    statement.executeUpdate();
+                }
             }
+            
+            // If the insert queries were queued in a batch, execute it.
+            if (supportsBatch)
+            {
+                statement.executeBatch();
+            }
+            
+            ConnectionHelper.cleanup(statement);
             
             // Commit transaction.
             connection.commit();
