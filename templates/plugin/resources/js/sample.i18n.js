6b370e36591f76ef850eda75c58e24333491537e
Raphaël Franchet <raphael.franchet@ametys.org>
2012-12-05 17:01:32 +0100

    RUNTIME-768 .i18n.js files are hard to read RUNTIME-803 admins log screen purge

--- templates/plugin/resources/js/sample.i18n.js
+++ templates/plugin/resources/js/sample.i18n.js
@@ -28,7 +28,7 @@ function myI18nFunction()
   alert("<i18n:text i18n:key="PLUGINS_PLUGIN_MYJAVASCRIPTKEY"/>");
   
   var test = true;
-  for (var i = 0; i &lt; 5 &amp;&amp; test; i++)
+  for (var i = 0; i < 5 && test; i++)
   {
     test = i == 3;
   }
