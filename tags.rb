4c49c327d759ccf211cb5b5fed01bf2c054de115
Raphaël Franchet <raphael.franchet@ametys.org>
2013-05-30 17:49:22 +0200

    RUNTIME-716 Add a js doc

--- tags.rb
+++ tags.rb
@@ -3,7 +3,7 @@ require "jsduck/tag/boolean_tag"
 class Ametys < JsDuck::Tag::BooleanTag
   def initialize
     @pattern = "ametys"
-    @signature = {:long => "ametys", :short => "ametys"}
+    @signature = {:long => "Ametys", :short => "Ame"}
     super
   end
 end
