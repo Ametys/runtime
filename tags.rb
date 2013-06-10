31526df339ebab0bf9bedfbfb9aafa5d1d606a1f
Raphaël Franchet <raphael.franchet@ametys.org>
2013-06-10 10:30:23 +0200

    moving to deliver

--- tags.rb
+++
@@ -1,9 +0,0 @@
-require "jsduck/tag/boolean_tag"
-
-class Ametys < JsDuck::Tag::BooleanTag
-  def initialize
-    @pattern = "ametys"
-    @signature = {:long => "Ametys", :short => "Ame"}
-    super
-  end
-end
