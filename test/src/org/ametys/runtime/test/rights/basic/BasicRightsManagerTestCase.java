7f54a45567e9c6186e97f8143506b78f8b378b96
Cédric Damioli <cedric.damioli@ametys.org>
2013-08-06 12:37:09 +0200

    test case failure since rights were added to the plugin core

--- test/src/org/ametys/runtime/test/rights/basic/BasicRightsManagerTestCase.java
+++ test/src/org/ametys/runtime/test/rights/basic/BasicRightsManagerTestCase.java
@@ -60,8 +60,8 @@ public class BasicRightsManagerTestCase extends AbstractRuntimeTestCase
         
         // GET USER RIGHTS
         Set<String> rightsIds = rightsManager.getUserRights("any", "any");
-        // one right from a test feature
-        assertEquals(1, rightsIds.size());
+        // one right from a test feature, and two from the plugin core
+        assertEquals(3, rightsIds.size());
         assertTrue(rightsIds.contains("Test_Right"));
         
         // GET GRANTED USERS
