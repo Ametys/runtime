f37cda6271bc4a570b67715d1ea1cffc9d931a16
Raphaël Franchet <raphael.franchet@ametys.org>
2013-10-17 10:11:09 +0200

    RUNTIME-960 Shutdown screen is broken

--- main/kernel/resources/js/Ametys.i18n.js
+++ main/kernel/resources/js/Ametys.i18n.js
@@ -417,6 +417,11 @@ Ext.define(
 			title = title || "<i18n:text i18n:key='KERNEL_SHUTDOWN_DEFAULTTITLE'/>";
 			message = message || "<i18n:text i18n:key='KERNEL_SHUTDOWN_DEFAULTTEXT'/>";
 			
+			if (console)
+			{
+				console.info("Shuting down Ametys '" + title + "' - " + message);
+			}
+			
 			// Shutdown request
 			Ametys.data.ServerComm._shutdown();
 			
@@ -448,22 +453,41 @@ Ext.define(
 			Ext.ComponentManager.each(function(id, item, size) {
 				if (item.ownerCt)
 				{
-					item.ownerCt.removeAll();
+					try
+					{
+						item.ownerCt.removeAll();
+					}
+					catch (e)
+					{
+						console.warn(e);
+					}
 				}
 			});
 			// Some special components are still there
 			Ext.ComponentManager.each(function(id, item, size) {
 				if (item.ownerCt)
 				{
-					item.ownerCt.remove(item, true); 
+					try
+					{
+						item.ownerCt.remove(item, true); 
+					}
+					catch (e)
+					{
+						console.warn(e);
+					}
 				}
 			});
 			// Some components are still there, but does not seems to have effets
 			Ext.app.Application.instance.suspendEvents(false);
 			
 			document.body.parentNode.setAttribute('style', "height: 100%;");
-			document.body.setAttribute('style', "background: -webkit-linear-gradient( #351e3b, #033059); background: -moz-linear-gradient( #351e3b, #033059); background: -ms-linear-gradient( #351e3b, #033059); background: -o-linear-gradient( #351e3b, #033059); background: linear-gradient( #351e3b, #033059); background-image: none !important; background-color: #351e3b !important; color: #000000 !important; padding: 20px !important; height: 100%;");
+			document.body.setAttribute('style', "background: -webkit-linear-gradient( #351e3b, #033059); background: -moz-linear-gradient( #351e3b, #033059); background: -ms-linear-gradient( #351e3b, #033059); background: -o-linear-gradient( #351e3b, #033059); background: linear-gradient( #351e3b, #033059); background-image: none !important; background-color: #351e3b !important; color: #000000 !important; padding: 20px !important; margin: 0 !important; height: 100%; -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box");
 			document.body.innerHTML = "<div style='width: 500px; margin: 200px auto 0 auto; background-color: #EFEFEF; border: 1px solid #CFCFCF; border-radius: 15px; padding: 0 15px;'><h1 style='font-size: 1.5em'>" + title + "</h1> <p>" + message + "</p><p style='text-align: center'><a href='javascript:Ametys.reload();'><i18n:text i18n:key='KERNEL_SERVERCOMM_LISTENERREQUEST_LOST_CONNECTION_3'/></a></p></div>"
+			
+			if (console)
+			{
+				console.info("Ametys shutdown");
+			}
 		},
 		
 		/**
