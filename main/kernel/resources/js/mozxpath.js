ac588adeef796e0038fa93e9e28ddc6f58e7e8fd
Raphaël Franchet <raphael.franchet@ametys.org>
2014-03-03 16:59:02 +0100

    Merged revisions 25751 via svnmerge from https://svn.ametys.org/trunk/runtime/trunk
    ........
      r25751 | raphael | 2014-03-03 16:56:50 +0100 (lun., 03 mars 2014) | 1 line
      
      CMS-5327 Chrome 34 will not work
    ........

--- main/kernel/resources/js/mozxpath.js
+++ main/kernel/resources/js/mozxpath.js
@@ -31,6 +31,35 @@ if( document.implementation.hasFeature("XPath", "3.0") )
 			return null;
 		}
 	}
+	
+	Document.prototype.selectNodes = function(cXPathString, xNode)
+	{
+		if( !xNode ) { xNode = this; } 
+
+		var oNSResolver = this.createNSResolver(this.documentElement)
+		var aItems = this.evaluate(cXPathString, xNode, oNSResolver, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null)
+		var aResult = [];
+		for( var i = 0; i < aItems.snapshotLength; i++)
+		{
+			aResult[i] =  aItems.snapshotItem(i);
+		}
+		
+		return aResult;
+	}
+	Document.prototype.selectSingleNode = function(cXPathString, xNode)
+	{
+		if( !xNode ) { xNode = this; } 
+
+		var xItems = this.selectNodes(cXPathString, xNode);
+		if( xItems.length > 0 )
+		{
+			return xItems[0];
+		}
+		else
+		{
+			return null;
+		}
+	}
 
 	Element.prototype.selectNodes = function(cXPathString)
 	{
