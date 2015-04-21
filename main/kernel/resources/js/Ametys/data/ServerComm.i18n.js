0c308171f9eb3fdb7b4a93f5ac5ac889faf70069
Raphaël Franchet <raphael.franchet@ametys.org>
2015-04-21 18:27:42 +0200

    CMS-6050 Infinite loop on #showRefreshed of a tool

--- main/kernel/resources/js/Ametys/data/ServerComm.i18n.js
+++ main/kernel/resources/js/Ametys/data/ServerComm.i18n.js
@@ -254,6 +254,7 @@ Ext.define(
 						if (Ext.isObject(oldMessage.waitMessage))
 						{
 							oldMessage.bodyMasked ? Ext.getBody().unmask() : oldMessage.waitMessage.hide();
+                            Ext.destroy(oldMessage.waitMessage);
 						}
 					}
 				}
@@ -875,4 +876,3 @@ Ext.define(
 		}
 	}
 );
-
