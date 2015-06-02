d5743f99107f36122aae1b2a7bfc3950513699f8
Laurence Aumeunier <laurence.aumeunier@ametys.org>
2015-06-02 12:06:37 +0200

    RUNTIME-1160 Migrate Logs Screen

--- main/plugin-core/src/org/ametys/runtime/plugins/core/administrator/logs/LogsGenerator.java
+++ main/plugin-core/src/org/ametys/runtime/plugins/core/administrator/logs/LogsGenerator.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2012 Anyware Services
+ *  Copyright 2015 Anyware Services
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -18,10 +18,7 @@ package org.ametys.runtime.plugins.core.administrator.logs;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -30,14 +27,9 @@ import java.util.regex.Pattern;
 
 import org.apache.cocoon.ProcessingException;
 import org.apache.cocoon.generation.ServiceableGenerator;
-import org.apache.cocoon.xml.AttributesImpl;
 import org.apache.cocoon.xml.XMLUtils;
 import org.apache.excalibur.source.SourceException;
 import org.apache.excalibur.source.TraversableSource;
-import org.apache.log4j.Level;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-import org.apache.log4j.spi.LoggerRepository;
 import org.xml.sax.SAXException;
 
 import org.ametys.runtime.util.parameter.ParameterHelper;
@@ -52,60 +44,12 @@ public class LogsGenerator extends ServiceableGenerator
     public void generate() throws IOException, SAXException, ProcessingException
     {
         contentHandler.startDocument();
-        XMLUtils.startElement(contentHandler, "Logger");
-
-        XMLUtils.startElement(contentHandler, "Logs");
+        XMLUtils.startElement(contentHandler, "logs");
         _logs();
-        XMLUtils.endElement(contentHandler, "Logs");
-
-        XMLUtils.startElement(contentHandler, "LogLevels");
-        _log4jLevels();
-        XMLUtils.endElement(contentHandler, "LogLevels");
-
-        XMLUtils.endElement(contentHandler, "Logger");
+        XMLUtils.endElement(contentHandler, "logs");
         contentHandler.endDocument();
     }
     
-    private void _log4jLevels() throws SAXException
-    {
-        try
-        {
-            LoggerRepository loggerRepository = LogManager.getLoggerRepository();
-            List<Logger> loggers = new ArrayList<>();
-            Enumeration<org.apache.log4j.Logger> enumLogger = loggerRepository.getCurrentLoggers();
-            
-            while (enumLogger.hasMoreElements())
-            {
-                loggers.add(enumLogger.nextElement());
-            }
-            
-            loggers.add(loggerRepository.getRootLogger());
-            
-            Collections.sort(loggers, new LoggerComparator());
-            
-            for (Logger logger : loggers)
-            {
-                String category = logger.getName();
-                Level level = logger.getLevel();
-                
-                AttributesImpl attrs = new AttributesImpl();
-                attrs.addCDATAAttribute("category", category);
-                attrs.addCDATAAttribute("priority", level == null ? "inherit" : level.toString());
-                
-                XMLUtils.createElement(contentHandler, "logger", attrs);
-            }
-        }
-        catch (SAXException e)
-        {
-            throw e;
-        }
-        catch (Exception e) 
-        {
-             // nothing to do
-            getLogger().warn("Unable to access internal logger properties", e);
-        }
-    }
-
     private void _logs() throws IOException, SAXException
     {
         TraversableSource logsDirectorySource = (TraversableSource) resolver.resolveURI("context://WEB-INF/logs");
@@ -116,21 +60,17 @@ public class LogsGenerator extends ServiceableGenerator
             
             for (String logname : logs.keySet())
             {
-                AttributesImpl logattrs = new AttributesImpl();
-                logattrs.addCDATAAttribute("name", logname);
-                XMLUtils.startElement(contentHandler, "Log", logattrs);
-                
                 List<TraversableSource> logSources = logs.get(logname);
                 for (TraversableSource logSource : logSources)
                 {
-                    XMLUtils.startElement(contentHandler, "file");
+                    XMLUtils.startElement(contentHandler, "log");
+                    XMLUtils.createElement(contentHandler, "name", logname);
                     XMLUtils.createElement(contentHandler, "location", logSource.getURI().substring(logSource.getURI().lastIndexOf('/') + 1));
                     XMLUtils.createElement(contentHandler, "lastModified", ParameterHelper.valueToString(new Date(logSource.getLastModified())));
                     XMLUtils.createElement(contentHandler, "size", Long.toString(logSource.getContentLength()));
-                    XMLUtils.endElement(contentHandler, "file");
+                    XMLUtils.endElement(contentHandler, "log");
                 }
                 
-                XMLUtils.endElement(contentHandler, "Log");
             }
         }
         finally
@@ -141,7 +81,7 @@ public class LogsGenerator extends ServiceableGenerator
     
     private Map<String, List<TraversableSource>> _prepareLogs(TraversableSource logsDirectorySource) throws SourceException
     {
-        Map<String, List<TraversableSource>> logs = new HashMap<>();
+        Map<String, List<TraversableSource>> logs = new HashMap<String, List<TraversableSource>>();
         
         Collection<TraversableSource> logFiles = logsDirectorySource.getChildren();
         for (TraversableSource logSource : logFiles)
@@ -156,7 +96,7 @@ public class LogsGenerator extends ServiceableGenerator
                     List<TraversableSource> underLogs = logs.get(canonicalName);
                     if (underLogs == null)
                     {
-                        underLogs = new ArrayList<>();
+                        underLogs = new ArrayList<TraversableSource>();
                         logs.put(canonicalName, underLogs);
                     }
                     
@@ -182,47 +122,6 @@ public class LogsGenerator extends ServiceableGenerator
         else
         {
             return null;
-        }
-    }
-    
-    /**
-     * Comparator to compare two loggers by their name 
-     */
-    public class LoggerComparator implements Comparator<Logger>
-    {
-        public int compare(Logger o1, Logger o2)
-        {
-            String[] o1NameParts = o1.getName().split("\\.");
-            String[] o2NameParts = o2.getName().split("\\.");
-            int i = 0;
-            
-            for (; i < o1NameParts.length && i < o2NameParts.length; i++)
-            {
-                int compare = o1NameParts[i].compareTo(o2NameParts[i]);
-                
-                if (compare != 0)
-                {
-                    return compare;
-                }
-                
-                // Same category, continue
-            }
-            
-            if (i == o1NameParts.length)
-            {
-                if (i != o2NameParts.length)
-                {
-                    // o2 has a longer category
-                    return 1;
-                }
-            }
-            else
-            {
-                // o1 has a longer category
-                return -1;
-            }
-            
-            return o1.getName().compareTo(o2.getName());
         }
     }
 }
