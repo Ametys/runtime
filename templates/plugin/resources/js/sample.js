594640ae3d828c28dd0f39e84d77b822a3c67fe8
Laurence Aumeunier <laurence.aumeunier@ametys.org>
2015-06-11 12:36:15 +0200

    RUNTIME-1190 Split runtime in 4 plugins

--- templates/plugin/resources/js/sample.js
+++ templates/plugin/resources/js/sample.js
@@ -1,22 +1,31 @@
-/*
- *  Copyright 2009 Anyware Services
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
- 
-function myFunction()
+<!--
+   Copyright 2009 Anyware Services
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   -->
+
+<!-- 
+  This sample of js will be xmlized so you cannot use xml special characters 
+  This comment will not be part of the output
+  The url to call this file is 'resources/js/sample.js'
+  
+  I18n namespace will be automatically added so you can use i18n tags
+  and default catalogue will be the plugin one
+-->
+
+function myI18nFunction()
 {
-  alert("Not internationalized message");
+  alert("<i18n:text i18n:key='PLUGINS_PLUGIN_MYJAVASCRIPTKEY'/>");
   
   var test = true;
   for (var i = 0; i < 5 && test; i++)
