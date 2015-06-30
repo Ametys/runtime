e460c25fec7afcc2022cb63765604aeab1173463
Raphaël Franchet <raphael.franchet@ametys.org>
2015-06-30 16:59:59 +0200

    RUNTIME-1240 List and documents all widgettypes for widgetmanager and the associated jstype

--- main/plugin-core-ui/resources/js/Ametys/form/WidgetManager.js
+++ main/plugin-core-ui/resources/js/Ametys/form/WidgetManager.js
@@ -15,93 +15,110 @@
  */
 
 /**
- * This class is the main class to register and retrieve widgets for edition
- * 
- * 		Ametys.form.WidgetManager.registerWidget(MyWidgetXType, MyWidgetXType, IsEnumeration, IsMultiple)
- * 		Ametys.form.WidgetManager.getWidget(MyWidgetXType, MyWidgetXType, MyWidgetConfig)
+ * This class is the main class to register and retrieve widgets for edition.
+ * It associates types to widgets.
+ * See constants for the lists of tabs.
  */
 Ext.define('Ametys.form.WidgetManager', {
 	singleton: true,
 	
 	/**
-	 * @property {String} TYPE_STRING The type 'string'
-	 * @private
+	 * @property {String} TYPE_STRING The type 'string'. A widget of such type should get/set values using the js type "string"
 	 * @readonly 
 	 */
 	TYPE_STRING: 'string',
+    /**
+     * @property {String} TYPE_PASSWORD The type 'password'. A widget of such type should get/set values using the js type "string". Note that the current real value should not be set and that getValue should return null to specify an unchanged password (while "" means a new empty password).
+     * @readonly 
+     */
+    TYPE_PASSWORD: 'password',
 	/**
-	 * @property {String} TYPE_GEOCODE The type 'geocode'
-	 * @private
+	 * @property {String} TYPE_GEOCODE The type 'geocode'. A widget of such type should get/set values using the js type "object" with the following properties:
+     * 
+     * * {Number} latitude The latitude value of the geocode
+     * * {Number} longitude The longitude value of the geocode
+     * 
 	 * @readonly 
 	 */
 	TYPE_GEOCODE: 'geocode',
 	/**
-	 * @property {String} TYPE_DATE The type 'date'
-	 * @private
+	 * @property {String} TYPE_DATE The type 'date'. A widget of such type should get/set values using the js type "date"
 	 * @readonly 
 	 */
 	TYPE_DATE: 'date',
 	/**
-	 * @property {String} TYPE_DATETIME The type 'datetime'
-	 * @private
+	 * @property {String} TYPE_DATETIME The type 'datetime'. A widget of such type should get/set values using the js type "date"
 	 * @readonly 
 	 */
 	TYPE_DATETIME: 'datetime',
 	/**
-	 * @property {String} TYPE_LONG The type 'long'
-	 * @private
+	 * @property {String} TYPE_LONG The type 'long'. A widget of such type should get/set values using the js type "number"
 	 * @readonly 
 	 */
 	TYPE_LONG: 'long',	
 	/**
-	 * @property {String} TYPE_DOUBLE The type 'double'
-	 * @private
+	 * @property {String} TYPE_DOUBLE The type 'double'. A widget of such type should get/set values using the js type "number"
 	 * @readonly 
 	 */
 	TYPE_DOUBLE: 'double',	
 	/**
-	 * @property {String} TYPE_BOOLEAN The type 'boolean'
-	 * @private
+	 * @property {String} TYPE_BOOLEAN The type 'boolean'. A widget of such type should get/set values using the js type "boolean"
 	 * @readonly 
 	 */
 	TYPE_BOOLEAN: 'boolean',
 	/**
-	 * @property {String} TYPE_RICH_TEXT The type 'rich_text'
-	 * @private
+	 * @property {String} TYPE_RICH_TEXT The type 'rich_text'. A widget of such type should get/set values using the js type "string" representing HTML code. See default implementation for details on how to store local files (images...) and awaited format for all tags.
 	 * @readonly 
 	 */
 	TYPE_RICH_TEXT: 'rich_text',
 	/**
-	 * @property {String} TYPE_BINARY The type 'binary'
-	 * @private
-	 * @readonly 
-	 */
-	TYPE_BINARY: 'binary',
-	/**
-	 * @property {String} TYPE_FILE The type 'file'
-	 * @private
+	 * @property {String} TYPE_FILE The type 'file'. A widget of such type should get/set values using the js type "object" with the following properties (if no file is selected the value should be {}):
+     * 
+     * * {String} type The kind of file on the server side. Such as "metadata" for a local file.
+     * * {String} [path] The path of the file (related to its type). Such as "attachments/1/attachment".
+     * * {String} filename The file name. Such as "image.png".
+     * * {String} [mimeType] The file metype. Such as "image/png".
+     * * {String} size The size in byte of the file
+     * * {String} [lastModified] The date of the last modification using the full ISO8601 format Ext.date#patterns.ISO8601DateTime
+     * * {String} id The internal identifier of the new file or "untouched" for an existing file. For example, a just uploaded file will have the temporary value assigned by the server in the upload directory ; whereas for a jcr resource it will be the jcr UUID.
+     * * {String} downloadUrl The absolute url to download the file
+     * * {String} viewUrl The absolute url to preview the file
+     *  
 	 * @readonly 
 	 */
 	TYPE_FILE: 'file',
 	/**
-	 * @property {String} TYPE_CONTENT The type 'content'
-	 * @private
-	 * @readonly 
-	 */
-	TYPE_CONTENT: 'content',
-	/**
-	 * @property {String} TYPE_SUB_CONTENT The type 'sub_content'
-	 * @private
-	 * @readonly 
-	 */
-	TYPE_SUB_CONTENT: 'sub_content',
-	/**
-	 * @property {String} TYPE_USER The type 'user'
-	 * @private
+	 * @property {String} TYPE_USER The type 'user'. A widget of such type should get/set values using the js type "string" representing the login of the user
 	 * @readonly 
 	 */
 	TYPE_USER: 'user',
+    /**
+     * @property {String} TYPE_REFERENCE The type 'reference'. A widget of such type should get/set values using the js type "object" with the following properties:
+     * 
+     * * {String} value The value of the reference. Can be an url or an identifier depending on the type.
+     * * {String} type The type of reference.
+     * 
+     * @readonly 
+     */
+    TYPE_REFERENCE: 'reference',
+    /**
+     * @property {String} TYPE_URL The type 'url'. A widget of such type should get/set values using the js type "string" representing the url.
+     * @readonly 
+     */
+    TYPE_URL: 'url',
 	
+    /**
+     * @property {String} TYPE_CONTENT The type 'content'. A widget of such type should get/set values using the js type "string" representing the content identifier
+     * @readonly 
+     */
+    TYPE_CONTENT: 'content',
+    /**
+     * @property {String} TYPE_SUB_CONTENT The type 'sub_content'. A widget of such type should get/set values using the js type "string" representing the content identifier
+     * @readonly 
+     */
+    TYPE_SUB_CONTENT: 'sub_content',
+    
+    
 	/**
 	 * @property {Object} _widgets The registered widgets classified by single/multiple property and type
 	 * @private
