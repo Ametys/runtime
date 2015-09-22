63e18c22adb2d11519bd1fcb72669bf68d8f0740
Quentin Glinel-Mortreuil <quentin.glinel-mortreuil@ametys.org>
2015-09-22 15:36:04 +0200

    CMS-6497 Register tools for the history tool

--- main/plugin-core/resources/js/Ametys/plugins/core/users/UsersTool.js
+++ main/plugin-core/resources/js/Ametys/plugins/core/users/UsersTool.js
@@ -71,27 +71,6 @@ Ext.define('Ametys.plugins.core.users.UsersTool', {
 		Ametys.message.MessageBus.on(Ametys.message.Message.DELETED, this._onMessageDeleted, this);
 	},
 	
-	setParams: function(params)
-	{
-    	// Register the tool on the history tool
-		var role = this.getFactory().getRole();
-	    var toolParams = this.getParams();
-
-        Ametys.navhistory.HistoryDAO.addEntry({
-			id: this.getId(),
-			label: this.getTitle(),
-			description: this.getDescription(),
-			iconSmall: this.getSmallIcon(),
-			iconMedium: this.getMediumIcon(),
-			iconLarge: this.getLargeIcon(),
-			type: Ametys.navhistory.HistoryDAO.TOOL_TYPE,
-			action: Ext.bind(Ametys.tool.ToolsManager.openTool, Ametys.tool.ToolsManager, [role, toolParams], false)
-        });
-        
-        this.callParent(arguments); 
-	},
-	
-	
 	getMBSelectionInteraction: function() 
 	{
 		return Ametys.tool.Tool.MB_TYPE_ACTIVE;
@@ -275,6 +254,21 @@ Ext.define('Ametys.plugins.core.users.UsersTool', {
 
 	setParams: function(params)
 	{
+    	// Register the tool on the history tool
+		var role = this.getFactory().getRole();
+	    var toolParams = this.getParams();
+
+        Ametys.navhistory.HistoryDAO.addEntry({
+			id: this.getId(),
+			label: this.getTitle(),
+			description: this.getDescription(),
+			iconSmall: this.getSmallIcon(),
+			iconMedium: this.getMediumIcon(),
+			iconLarge: this.getLargeIcon(),
+			type: Ametys.navhistory.HistoryDAO.TOOL_TYPE,
+			action: Ext.bind(Ametys.tool.ToolsManager.openTool, Ametys.tool.ToolsManager, [role, toolParams], false)
+        });
+        
 		this.callParent(arguments);
 		this.search();
 	},
