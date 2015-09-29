ccf0a0ae75e8e813d22d95aa6d0576c6e7859787
Grégory Lefebvre <gregory.lefebvre@ametys.org>
2015-09-29 14:34:45 +0200

    SKINFACTORY-81 Migrate Skin Factory for Ametys v4

---
+++ main/plugin-core-ui/resources/js/Ametys/form/field/ColorSelector.js
@@ -0,0 +1,263 @@
+/*
+ *  Copyright 2015 Anyware Services
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/**
+ * Field for selecting a color value from a list of available colors, or from a  
+ */
+Ext.define('Ametys.form.field.ColorSelector', {
+	extend: 'Ametys.form.AbstractFieldsWrapper',
+	
+	layout: 'vbox',
+	
+	/**
+	 * ·@cfg {Object/Object[]} listColors An array of color objects, with properties :
+	 * {String} listColors.label The name of the picker
+	 * {String[]} [listColors.colors] The list of colors code, such as "FF0000".
+	 * If listColors is undefined, it will assume the default color configuration.
+	 */
+	
+	/**
+	 * @cfg {boolean} allowTransparent True to allow selection of a fully transparent color
+	 */
+	
+	/**
+	 * @cfg {String} value @inheritdoc Format is a 6 hex characters color value, or 'transparent'.
+	 */
+	
+	/**
+	 * @cfg {Function} callback The function called when a color is selected. Arguments is :
+	 * {String} callback.color The 6 hex characters color value, or 'transparent'.
+	 */
+	
+	initComponent: function() 
+	{
+		this.items = [];
+		
+		this.listColors = Ext.Array.from(this.listColors);
+		
+		if (this.listColors.length > 0)
+		{
+			Ext.Array.each(this.listColors, this._initColorPicker, this);
+		}
+		else
+		{
+			this._initColorPicker({
+				label: "<i18n:text i18n:key='PLUGINS_CORE_UI_COLORSELECTOR_DEFAULT_COLORS'/>"
+			});
+		}
+		
+		if (this.allowTransparent)
+		{
+			this.items.push({
+				xtype: 'button',
+				itemId: 'buttonTransparent',
+				icon: Ametys.getPluginResourcesPrefix('skinfactory') + '/img/transparent_22.png',
+				text: "<i18n:text i18n:key='PLUGINS_CORE_UI_COLORSELECTOR_NO_COLOR'/>",
+				width: 145,
+				handler: this._setTransparent,
+				scope: this
+			});
+		}
+		
+		this.items.push({
+			xtype: "panel",
+			itemId: "customColorsPanel",
+			title: "<i18n:text i18n:key='PLUGINS_CORE_UI_COLORSELECTOR_CUSTOM_COLORS'/>",
+			hidden: true,
+			items: []
+		});
+		
+		this.items.push({
+			xtype: 'button',
+			itemId: 'buttonOthers',
+			text: "<i18n:text i18n:key='PLUGINS_CORE_UI_COLORSELECTOR_OTHERS_COLORS'/>",
+			width: 145,
+			handler: this._openOtherColors,
+			scope: this
+		});
+		
+		this.callParent(arguments);
+	},
+	
+	onRender: function ()
+	{
+		var currentColor = this.getInitialConfig("currentColor");
+		if (currentColor)
+		{
+			this.setColor(currentColor);
+		}
+		
+		this.callParent(arguments);
+	},
+	
+	/**
+	 * Initialize a new color picker and it header
+	 * @param {Object} colorInfos The color picker infos
+	 * @param {String} [colorInfos.label] The header text. If ommited, the header is not created.
+	 * @private
+	 */
+	_initColorPicker: function (colorInfos) 
+	{
+		this.items.push({
+			xtype: "panel",
+			title: colorInfos.label,
+			items: [
+				Ext.applyIf({
+					xtype: 'colorpicker',
+					handler: this._onColorPickerSelect,
+					scope: this,
+					height: "auto"
+				}, colorInfos)
+			]
+		});
+	},
+	
+	/**
+	 * Handler for the transparent button. Set the color "transparent"
+	 * @private
+	 */
+	_setTransparent: function ()
+	{
+		this.setColor("transparent");
+		
+		var cbFn = this.getInitialConfig("callback");
+		if (Ext.isFunction(cbFn))
+		{
+			cbFn("transparent");
+		}
+	},
+	
+	/**
+	 * Handler for the "other colors" button. Open the helper to pick other colors
+	 * @private
+	 */
+	_openOtherColors: function ()
+	{
+		var color = this._currentColor == "transparent" ? "000000" : this._currentColor;
+		Ametys.helper.ChooseColor.open(this._currentColor, Ext.bind(this._onColorSelect, this));
+	},
+	
+	/**
+	 * Set the current value of the field.
+	 * @param {String} color The color
+	 */
+	setColor: function (color)
+	{
+		var colorPickers = this.query("colorpicker");
+		
+		var valueExists = false;
+		// update the color pickers
+		Ext.Array.each(colorPickers, function (picker) {
+			if (Ext.Array.contains(picker.colors, color))
+			{
+				// select the color
+				if (picker.itemId != "customColors")
+				{
+					picker.select(color);
+					valueExists = true;
+				}
+				else
+				{
+					picker.colors = Ext.Array.remove(picker.colors, color);
+					picker.clear();
+				}
+			}
+			else
+			{
+				picker.clear();
+			}
+		});
+		
+		// update the transparency button
+		var buttonTransparent = this.down("#buttonTransparent");
+		buttonTransparent.toggle(color == "transparent", true);
+		if (color == "transparent")
+		{
+			valueExists = true;
+			buttonTransparent.setIcon(Ametys.getPluginResourcesPrefix('skinfactory') + '/img/transparent_selected_22.png');
+		}
+		else
+		{
+			buttonTransparent.setIcon(Ametys.getPluginResourcesPrefix('skinfactory') + '/img/transparent_22.png');
+		}
+		
+		if (!valueExists)
+		{
+			// update the custom colors
+			var pickerPanel = this.getComponent("customColorsPanel");
+			pickerPanel.setHidden(false);
+			
+			var picker = pickerPanel.getComponent("customColors");
+			var colors;
+			if (picker)
+			{
+				colors = Ext.Array.insert(picker.colors, 0, [color]);
+				// limit to 8 elements (1 row)
+				if (colors.length > 8)
+				{
+					colors = Ext.Array.slice(colors, 0, 8);
+				}
+			
+				pickerPanel.remove(picker);
+			}
+			else
+			{
+				colors = [color];
+			}
+			
+			picker = Ext.create('Ext.picker.Color', {
+				colors: colors,
+				height: "auto",
+				itemId: "customColors",
+				value: color,
+				handler: this._onColorPickerSelect,
+				scope: this
+			});
+			
+			pickerPanel.add(picker);
+		}
+		
+		this._currentColor = color;
+	},
+	
+	/**
+	 * Handler for the color pickers. 
+	 * @param {Ext.picker.color} object The color picker that initiated the action.
+	 * @param {String} color The color value
+	 * @private
+	 */
+	_onColorPickerSelect: function (object, color)
+	{
+		this._onColorSelect(color);
+	},
+	
+	/**
+	 * Handler for the color selection
+	 * @param {String} color The color value
+	 * @private
+	 */
+	_onColorSelect: function (color)
+	{
+		this.setColor(color.toUpperCase());
+		
+		var cbFn = this.getInitialConfig("callback");
+		if (Ext.isFunction(cbFn))
+		{
+			cbFn(color);
+		}
+	}
+	
+});
---
+++ main/plugin-core-ui/resources/js/Ametys/helper/ChooseColor.js
@@ -0,0 +1,125 @@
+/*
+ *  Copyright 2015 Anyware Services
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/**
+ * This helper is used to choose a color. See #open method.
+ */
+Ext.define("Ametys.helper.ChooseColor", {
+    singleton: true,
+
+    /**
+     * @private
+     * @property {Function} _cbFn Callback function to execute after the user validates the dialog
+     */
+
+    /**
+     * @private
+     * @property {Ametys.window.DialogBox} _box The dialog box
+     */
+
+    /**
+     * @private
+     * @property {Ext.form.FormPanel} _colorSelector The color selector
+     */
+
+    /**
+     * Open the dialog box for selecting a color
+     * @param {String} [color] The current color value, in hex6 format, with no leading '#'. Default value is "000000".
+     * @param {Function} [callback] The function to call when successful. This function receive the new color as argument, in hex6 format.
+     */
+    open: function (color, callback)
+    {
+        this._cbFn = callback;
+
+        this._delayedInitialize();
+
+        this._box.show();
+        this._initDialog(color || "000000");
+    },
+
+    /**
+     * Creates the dialog box
+     * @private
+     */
+    _delayedInitialize: function ()
+    {
+        if (this._initialized)
+        {
+            return;
+        }
+
+        this._colorSelector = Ext.create({
+            xtype: 'colorselector',
+			showPreviousColor: false,
+			showOkCancelButtons: false
+        });
+
+        this._box = Ext.create('Ametys.window.DialogBox', {
+            title: "<i18n:text i18n:key='PLUGINS_CORE_UI_COLORSELECTOR_OTHERS_COLORS'/>",
+            width: 600,
+            scrollable: false,
+
+            items: [this._colorSelector],
+
+            closeAction: 'hide',
+            buttons: [{
+                text: "<i18n:text i18n:key='PLUGINS_CORE_UI_HELPER_CHOOSECOLOR_OK'/>",
+                handler: Ext.bind(this._validate, this)
+            }, {
+                text: "<i18n:text i18n:key='PLUGINS_CORE_UI_HELPER_CHOOSECOLOR_CANCEL'/>",
+                handler: Ext.bind(this._cancel, this)
+            }]
+        });
+
+        this._initialized = true;
+    },
+
+
+    /**
+     * Initialize the dialog
+     * @param {String} [color] The current color value.
+     * @private
+     */
+    _initDialog: function (color)
+    {
+        this._colorSelector.setValue(color);
+    },
+
+   /**
+    * Handler for the 'Ok' button of the dialog box.
+    * Return the current color value to the callback
+    * @private
+    */
+    _validate: function ()
+    {
+        var color = this._colorSelector.getValue();
+
+        this._box.hide();
+        if (Ext.isFunction(this._cbFn))
+        {
+            this._cbFn(color);
+        }
+    },
+
+    /**
+     * Handler for the "cancel" button of the dialog. Hide the dialog.
+     * @private
+     */
+    _cancel: function ()
+    {
+        this._box.hide();
+    }
+});
