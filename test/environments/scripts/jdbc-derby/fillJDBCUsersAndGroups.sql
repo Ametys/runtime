2b19c3efdd786670bc620c5ab74636aa117d9f97
Raphaël Franchet <raphael.franchet@ametys.org>
2016-07-28 11:12:32 +0200

    Jdbc does not handle non-auth db

--- test/environments/scripts/jdbc-derby/fillJDBCUsersAndGroups.sql
+++ test/environments/scripts/jdbc-derby/fillJDBCUsersAndGroups.sql
@@ -15,9 +15,8 @@
 --
 
 insert into Users (login, firstname, lastname, email) values 
-('test', 'Test', 'TEST', 'test@test.te'),
-('test2', 'Test2', 'TEST2', 'test2@test.te');
-
+('test', 'Test', 'TEST', 'test@test.te', 'CY9rzUYh03PK3k6DJie09g==', null),
+('test2', 'Test2', 'TEST2', 'test2@test.te', '6151b762d08841411cf52184a2538cddaf3dc221914eda9198c236ec37a9f6a929f613b16a5c4bccfa483e7376f241212bf040a557addc5ef2adea9dc8c6f5e7', 'Eyp1pUMWKO9PKy67vYaEUezy2Zu2DQF72RG0qakzvoSgSX6r');
 insert into Groups (Label) values ('Group 1');
 insert into Groups_Users (Group_Id, Login, UserPopulation_Id) values 
 ((SELECT Id FROM Groups WHERE Id = IDENTITY_VAL_LOCAL()), 'test', 'population');
--- test/environments/scripts/jdbc-oracle/fillJDBCUsersAndGroups.sql
+++ test/environments/scripts/jdbc-oracle/fillJDBCUsersAndGroups.sql
@@ -14,10 +14,10 @@
 --  limitations under the License.
 --
 
-insert into Users (login, firstname, lastname, email) values 
-('test', 'Test', 'TEST', 'test@test.te');
-insert into Users (login, firstname, lastname, email) values 
-('test2', 'Test2', 'TEST2', 'test2@test.te');
+insert into Users (login, firstname, lastname, email, password, salt) values 
+('test', 'Test', 'TEST', 'test@test.te', 'CY9rzUYh03PK3k6DJie09g==', null);
+insert into Users (login, firstname, lastname, email, password, salt) values 
+('test2', 'Test2', 'TEST2', 'test2@test.te', '6151b762d08841411cf52184a2538cddaf3dc221914eda9198c236ec37a9f6a929f613b16a5c4bccfa483e7376f241212bf040a557addc5ef2adea9dc8c6f5e7', 'Eyp1pUMWKO9PKy67vYaEUezy2Zu2DQF72RG0qakzvoSgSX6r');
 
 insert into Groups (Id, Label) values (seq_groups.nextval, 'Group 1');
 insert into Groups_Users (Group_Id, Login, UserPopulation_Id) values 
