e09b7c9a759c425e79e5acddc44ca53191037a75
Raphaël Franchet <raphael.franchet@ametys.org>
2016-10-27 14:35:55 +0200

    RUNTIME-1984 Missing icon for the timeout dialog
    better use glyph

--- main/plugin-core-ui/resources/js/Ametys/data/ServerComm/TimeoutDialog.js
+++ main/plugin-core-ui/resources/js/Ametys/data/ServerComm/TimeoutDialog.js
@@ -96,7 +96,7 @@ Ext.define(
 	    		width: 530,
 	    		height: 120,
 	    		scrollable: true,
-	    		icon: Ametys.getPluginResourcesPrefix("core-ui") + "/img/Ametys/theme/gray/timeout_16.png",
+                iconCls: "ametysicon-arrow123",
 	    		items: [ centralMsg, detailledMsg ],
 	    		closeAction: 'close',
 	    		closable: false,
